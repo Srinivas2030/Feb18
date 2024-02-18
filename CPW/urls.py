bd312052d787d40c4a338a3774e26e612f71bf6f
SRINIVAS2030 <djsrinivas2030@gmail.com>
2024-02-18 17:49:20 +0530

    first

---
+++ CPW/urls.py
@@ -0,0 +1,38 @@
+"""
+URL configuration for CPW project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from . import views
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static 
+from django.conf import settings
+
+
+
+ 
+  
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('about_me/', views.about_me,name='about_me'),
+    path('home/',views.index2,name="home"),
+    path('analyze', views.analyze, name="analyze"),
+] 
+    
+    
