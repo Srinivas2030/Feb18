50ed16a5f1e58f66c01424a2301350da38dd473e
K Srinivas <94839346+Srinivas2030@users.noreply.github.com>
2024-07-03 04:32:17 +0530

    modifiying deploying files

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/blog
-sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/Feb18/nginx/nginx.conf /etc/nginx/sites-available/CPW
+sudo ln -s /etc/nginx/sites-available/CPW /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
